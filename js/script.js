a254a73419bc23cb07182d8d3049a84da04e19e4
Oleksii Chyzhaiev <jassderoo@gmail.com>
2024-08-05 03:06:47 +0300

    HW2 completed

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin)
+
+let namePlanet = "Terra";
+let nameUser = "John";
+
+let value = "10" + 10;
+console.log(value);
+console.log(typeof(value));
