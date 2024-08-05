e869ac86c5ccaff0ae7694dd7fb762dde64688ab
Oleksii Chyzhaiev <jassderoo@gmail.com>
2024-08-06 02:27:12 +0300

    HW3 completed

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const numberA = parseFloat(prompt('enter your numberA'));
+const numberB = parseFloat(prompt('enter your numberB'));
+
+
+const addition = (numberA + numberB);
+console.log(`Результат: ${numberA} + ${numberB} = ${addition}`);
+
+const subtraction = (numberA - numberB);
+console.log(`Результат: ${numberA} - ${numberB} = ${subtraction}`);
+
+const multiplication = (numberA * numberB);
+console.log(`Результат: ${numberA} * ${numberB} = ${multiplication}`);
+
+const division = (numberA / numberB);
+console.log(`Результат: ${numberA} / ${numberB} = ${division}`);
+
+
