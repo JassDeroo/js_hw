2dd2881179834e49433a18dd99aa2b0b85c9bf33
Oleksii Chyzhaiev <jassderoo@gmail.com>
2024-08-12 16:20:04 +0300

    HW8 completed

---
+++ js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const number1 = +prompt('enter first number');
+const number2 = +prompt('enter second number');
+const number3 = +prompt('enter third number');
+
+const averageValue = (number1 + number2 + number3) / 3;
+  alert(averageValue);
