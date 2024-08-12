678f09f98c7bc1748e6dcc1df297f2c847824888
Oleksii Chyzhaiev <jassderoo@gmail.com>
2024-08-12 15:16:45 +0300

    HW7 completed

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const hours= +prompt('enter hours amount');
+// console.log(typeof hours);
+
+const minutes = hours * 60;
+// alert(minutes);
+
+const seconds = minutes * 60;
+   alert(seconds);
